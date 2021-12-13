3cb14adfc5a46c0ed3aa064038a0db7474f6ab18
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2021-12-13 22:30:49 +0700

    feat(pages): add register page

--- pages/register.tsx
+++ pages/register.tsx
@@ -1,13 +1,46 @@
 import React from 'react'
 import { NextPage } from 'next'
-import { Container, Typography } from '@mui/material'
+import { useRouter } from 'next/router'
+import { Box, Button, Link, TextField, Typography } from '@mui/material'
+import AuthLayout from '@src/layouts/AuthLayout'
 
-const RegisterPage: NextPage = () => {
+const LoginPage: NextPage = () => {
+  const router = useRouter()
+
   return (
-    <Container>
-      <Typography>Register page</Typography>
-    </Container>
+    <AuthLayout>
+      <Box display="flex" flexDirection="column">
+        <Typography
+          color="primary"
+          variant="h5"
+          align="center"
+          marginBottom={6}
+        >
+          Register
+        </Typography>
+        <TextField label="Nama Lengkap" fullWidth />
+        <TextField label="Email" fullWidth />
+        <TextField label="Password" fullWidth type="password" />
+        <TextField
+          label="Ulangi Password"
+          fullWidth
+          style={{ marginBottom: '48px' }}
+          type="password"
+        />
+        <Button variant="contained" fullWidth>
+          Daftar
+        </Button>
+        <Typography
+          align="center"
+          variant="caption"
+          style={{ marginTop: '14px' }}
+        >
+          Sudah punya akun?{' '}
+          <Link onClick={() => router.push('login')}>Masuk</Link>
+        </Typography>
+      </Box>
+    </AuthLayout>
   )
 }
 
-export default RegisterPage
+export default LoginPage
