8baa6b325582f164cf67a53d79de7b6ee9fe5562
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2021-12-14 08:31:35 +0700

    feat(pages): add [id]/edit.tsx and remove edit-contact.tsx

--- pages/[id]/edit.tsx
+++ pages/[id]/edit.tsx
@@ -1,8 +1,15 @@
-import React from 'react'
+import React, { useMemo } from 'react'
 import { NextPage } from 'next'
 import { Container, Typography } from '@mui/material'
+import { useRouter } from 'next/router'
 
 const EditContactPage: NextPage = () => {
+  const router = useRouter()
+
+  const id = useMemo(() => {
+    return router.query.id
+  }, [router.query])
+
   return (
     <Container>
       <Typography>Edit contact</Typography>
