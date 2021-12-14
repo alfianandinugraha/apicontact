d0af02c4547efe4acdf3f35f6b2308ad59e264ce
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2021-12-14 09:53:06 +0700

    feat(pages): add design for profile page

--- pages/profile.tsx
+++ pages/profile.tsx
@@ -1,12 +1,93 @@
 import React from 'react'
 import { NextPage } from 'next'
-import { Container, Typography } from '@mui/material'
+import { Box, Button, Container, Typography } from '@mui/material'
+import BaseLayout from '@src/layouts/base-layout'
+import { Logout } from '@mui/icons-material'
+import TextField from '@src/components/text-field'
+import useTextField from '@src/hooks/use-text-field'
+import validator from 'validator'
+import { inputErrorMessage } from '@src/const/messages'
+import { useRouter } from 'next/router'
+
+const emailValidator = (e: any) => {
+  if (!validator.isEmail(e.value)) {
+    return inputErrorMessage.INVALID_EMAIL
+  }
+  return e.errorMessage
+}
 
 const ProfilePage: NextPage = () => {
+  const [inputName, nameHandler] = useTextField()
+  const [inputEmail, emailHandler] = useTextField({
+    validator: emailValidator,
+  })
+  const [inputPassword, passwordHandler] = useTextField()
+  const router = useRouter()
+
+  const submit = () => {
+    const isNameError = nameHandler.checkError()
+    const isEmailError = emailHandler.checkError()
+    const isPasswordError = passwordHandler.checkError()
+
+    if (isNameError || isEmailError || isPasswordError) {
+      console.log('Input error !')
+      return
+    }
+
+    console.log('Send request...')
+  }
+
   return (
-    <Container>
-      <Typography>Profile Page</Typography>
-    </Container>
+    <BaseLayout>
+      <Container sx={{ mt: '60px' }}>
+        <Box display="flex" justifyContent="space-between">
+          <Typography variant="h5">Akun Saya</Typography>
+          <Typography
+            variant="caption"
+            color="error"
+            sx={{ display: 'flex', alignItems: 'center', cursor: 'pointer' }}
+            onClick={() => router.push('login')}
+          >
+            Logout <Logout sx={{ width: '14px', height: '14px', ml: '4px' }} />
+          </Typography>
+        </Box>
+        <TextField
+          sx={{ mt: '40px' }}
+          fullWidth
+          label="Nama"
+          onChange={nameHandler.onChange}
+          value={inputName.value}
+          error={!!inputName.errorMessage}
+          errorMessage={inputName.errorMessage}
+        />
+        <TextField
+          fullWidth
+          label="Email"
+          onChange={emailHandler.onChange}
+          value={inputEmail.value}
+          error={!!inputEmail.errorMessage}
+          errorMessage={inputEmail.errorMessage}
+        />
+        <TextField
+          fullWidth
+          label="Password"
+          type="password"
+          onChange={passwordHandler.onChange}
+          value={inputPassword.value}
+          error={!!inputPassword.errorMessage}
+          errorMessage={inputPassword.errorMessage}
+        />
+        <Button
+          variant="contained"
+          color="primary"
+          fullWidth
+          sx={{ marginTop: '40px', mb: '70px' }}
+          onClick={submit}
+        >
+          Simpan
+        </Button>
+      </Container>
+    </BaseLayout>
   )
 }
 
