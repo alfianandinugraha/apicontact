8a4fb228fa5801c98b16d47fbbb104d807434a17
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2021-12-14 15:53:32 +0700

    feat(utils): add verify on jwt.ts

--- server/utils/jwt.ts
+++ server/utils/jwt.ts
@@ -1,4 +1,4 @@
-import jsonwebtoken from 'jsonwebtoken'
+import jsonwebtoken, { JwtPayload } from 'jsonwebtoken'
 
 const sign = (payload: object) => {
   return jsonwebtoken.sign(payload, process.env.NEXT_APP_JWT_SECRET_KEY ?? '', {
@@ -6,8 +6,22 @@ const sign = (payload: object) => {
   })
 }
 
+const verify = (token: string) => {
+  try {
+    const result = jsonwebtoken.verify(
+      token,
+      process.env.NEXT_APP_JWT_SECRET_KEY ?? ''
+    )
+
+    return result
+  } catch (err) {
+    return undefined
+  }
+}
+
 const jwt = {
   sign,
+  verify,
 }
 
 export default jwt
