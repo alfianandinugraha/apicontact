4613f1caa5a7e90b5b2c4890a81a0629428f3082
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2021-12-14 15:54:25 +0700

    feat(api): use middleware checkToken on contacts

--- pages/api/contacts.ts
+++ pages/api/contacts.ts
@@ -1,11 +1,17 @@
+import checkToken from '@server/middlewares/checkToken'
 import { NextApiRequest, NextApiResponse } from 'next'
 import nc from 'next-connect'
+import { User } from 'types'
 
-const handler = nc<NextApiRequest, NextApiResponse>().get((req, res) => {
-  return res.json({
-    message: 'Success get all contact',
-    body: [],
+const handler = nc<NextApiRequest, NextApiResponse>()
+  .use(checkToken)
+  .get((req, res) => {
+    const userInfo: User = req.body.userInfo
+
+    return res.json({
+      message: 'Success get all contact',
+      body: [],
+    })
   })
-})
 
 export default handler
