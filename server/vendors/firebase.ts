a28fa15208acffb076383530b9b8882f42c11f6f
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2021-12-15 16:08:54 +0700

    chore(firebase): change env variables

--- server/vendors/firebase.ts
+++ server/vendors/firebase.ts
@@ -2,12 +2,12 @@ import { initializeApp } from 'firebase/app'
 import { getFirestore, collection, getDoc, getDocs } from 'firebase/firestore'
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_APP_FIREBASE_APP_ID,
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
 }
 
 const firebase = initializeApp(firebaseConfig)
